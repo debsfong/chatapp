459ec5ec38f0259c52f5e3b8ec0a3c60e80eea4c
Debra Fong <debsfong@gmail.com>
2017-05-19 01:33:36 -0700

    rails action cable

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
+
+  root to: 'rooms#show'
+  # get 'rooms/show'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
